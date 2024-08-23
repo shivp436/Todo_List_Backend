b067109701e656a9924c53f29040dc9ab351814c
shivp436 <mpshivanandpatil2002@gmail.com>
2024-08-24 02:57:56 +0530

    Updated CORS IPs

--- src/middleware/corsMiddleware.ts
+++ src/middleware/corsMiddleware.ts
@@ -5,6 +5,7 @@ import sendResponse from '../utils/responseUtils';
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5000',
+  'http://localhost:5500',
   'http://localhost:8080',
   'http://localhost:8000',
 ];
